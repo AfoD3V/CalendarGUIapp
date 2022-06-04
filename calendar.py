3222349ffa5badd372a3d57f6e665b63fd705ceb
Roman A <romanafonin@protonmail.com>
2022-06-04 11:16:05 +0200

    Making test's with a use of unittest

--- calendar.py
+++ calendar.py
@@ -1,10 +1,13 @@
+import unittest
+
+
 class ListingStrategy:
     def __init__(self):
         self.events = []
 
     def register_event(self, event: tuple):
         """Function for registering new event on the list"""
-        self.events.append(event)
+        return self.events.append(event)
 
     def print_event(self, strategy: str):
         """General function for printing registered event in a format defined by strategy"""
@@ -27,34 +30,26 @@ def _standard_format_strategy(registered_event_list: list) -> list:
     return str_list
 
 
-assert _standard_format_strategy([]) == []
-assert _standard_format_strategy([('sample_title', '12.12.2020', '12:45')]) == (["Title: sample_title\n"
-                                                                                 "Date: 12.12.2020, 12:45"])
-
-
 def _icalendar_format_srategy(registered_event_list: list) -> list:
     """Function for inside use, preparing data for icalendar format"""
     # Start of string, always same for every case
-    str_list = ['''
-BEGIN:VCALENDAR
-VERSION:2.0
-BEGIN:VTIMEZONE
-TZID:Europe/Warsaw
-X-LIC-LOCATION:Europe/Warsaw
-END:VTIMEZONE
-    '''.rstrip()]
+    str_list = [
+        "\nBEGIN:VCALENDAR\n"
+        "VERSION:2.0\n"
+        "BEGIN:VTIMEZONE\n"
+        "TZID:Europe/Warsaw\n"
+        "X-LIC-LOCATION:Europe/Warsaw\n"
+        "END:VTIMEZONE".rstrip()
+                ]
 
     # Loop which is iterating through registered events
     for i in range(len(registered_event_list)):
         day, month, year = registered_event_list[i][1].split('.')
         hour, minute = registered_event_list[i][2].split(':')
-        middle_part = f'''
-BEGIN:VEVENT
-DTSTART:{year + month + day + "T" + hour + minute + "00"}
-DTEND:{year + month + day + "T" + hour + minute + "00"}
-SUMMARY:{registered_event_list[i][0]}
-END:VEVENT
-        '''.strip()
+        middle_part = f"BEGIN:VEVENT\n" \
+                      f"DTSTART:{year + month + day + 'T' + hour + minute + '00'}\n" \
+                      f"DTEND:{year + month + day + 'T' + hour + minute + '00'}\n" \
+                      f"SUMMARY:{registered_event_list[i][0]}\nEND:VEVENT".strip()
         str_list.append(middle_part)
 
     # Ending string, like start same for every case
@@ -65,14 +60,23 @@ END:VEVENT
         return str_list
 
 
-def testing_function 
-assert _icalendar_format_srategy([]) == []
+class TestCalendarMethods(unittest.TestCase):
 
-test_list = [
-    '\nBEGIN:VCALENDAR\nVERSION:2.0\nBEGIN:VTIMEZONE\nTZID:Europe/Warsaw\nX-LIC-LOCATION:Europe/Warsaw\nEND:VTIMEZONE',
-    'BEGIN:VEVENT\nDTSTART:20201212T124500\nDTEND:20201212T124500\nSUMMARY:sample_title\nEND:VEVENT',
-    'END:VCALENDAR\n'
-]
+    def test_standard_format(self):
+        # case_1
+        self.assertEqual(_standard_format_strategy([]), [], "standard_format case_1 failed")
+        # case_2
+        self.assertEqual(_standard_format_strategy([('sample_title', '12.12.2020', '12:45')]),
+                         ["Title: sample_title\nDate: 12.12.2020, 12:45"], "standard_format case_2 failed")
 
-assert _icalendar_format_srategy([('sample_title', '12.12.2020', '12:45')]) == test_list, (
-    f"\nExpected: {test_list}\n     Got: {_icalendar_format_srategy([('sample_title', '12.12.2020', '12:45')])}")
+    def test_icalendar_format(self):
+        # case_1
+        self.assertEqual(_icalendar_format_srategy([]), [], "icalendar_format case_1 failed")
+        test_list = [
+            '\nBEGIN:VCALENDAR\nVERSION:2.0\nBEGIN:VTIMEZONE\nTZID:Europe/Warsaw\nX-LIC-LOCATION:Europe/Warsaw\nEND:VTIMEZONE',
+            'BEGIN:VEVENT\nDTSTART:20201212T124500\nDTEND:20201212T124500\nSUMMARY:sample_title\nEND:VEVENT',
+            'END:VCALENDAR\n'
+        ]
+        # case_2
+        self.assertEqual(_icalendar_format_srategy([('sample_title', '12.12.2020', '12:45')]), test_list,
+                         "icalendar_format case_2 failed")
